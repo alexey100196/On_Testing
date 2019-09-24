0e3013216bfe9a42d140ea6c9e13dbecd14018e8
alexey100196 <alexeysobolev071@gmail.com>
2019-09-24 14:25:20 +0300

    add Todo vue and router

---
+++ vue_start_project/vue-starting/src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Home Page</h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, voluptatibus?</p>
+    <a href="/todos">Todos</a>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
---
+++ vue_start_project/vue-starting/src/views/Todos.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h2>odo application</h2>
+    <router-link to="/">Home</router-link>
+    <AddTodo
+      @add-todo="addTodo"
+    />
+    <TodoList
+      v-bind:todos="todos"
+      @remove-todo="removeTodo"
+    />
+
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      todos: [
+        // { id: 1, title: 'buy bread', completed: false },
+        // { id: 2, title: 'buy beer', completed: false },
+        // { id: 3, title: 'buy bike', completed: false }
+      ]
+    }
+  },
+  components: {
+    // HelloWorld
+    TodoList,
+    AddTodo
+  },
+  mounted () {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+      .then(response => response.json())
+      .then(json => {
+        this.todos = json
+      })
+  },
+  methods: {
+    removeTodo (id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    },
+    addTodo (todo) {
+      this.todos.push(todo)
+    }
+  }
+}
+</script>
